24f8830dd674f20dfa481eafe2efac259714658d
Crystal Finch <crystal.storm+finch@gmail.com>
2025-07-23 13:01:27 -0700

    Tweak authLayout styles

--- apps/frontend/src/app/components/auth-layout/AuthLayout.tsx
+++ apps/frontend/src/app/components/auth-layout/AuthLayout.tsx
@@ -1,10 +1,8 @@
-import heroImage from '../../../assets/images/unsplash.jpg';
-
 function AuthLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="h-screen p-4 box-border flex gap-4 items-center justify-center">
       {children}
-      <div className="bg-gray-100 grow h-full rounded-3xl w-2xl"></div>
+      <div className="bg-gray-200 grow h-full rounded-3xl w-2xl"></div>
     </div>
   );
 }
