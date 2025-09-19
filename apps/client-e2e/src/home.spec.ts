5c62edde0ff9680a8ef800854ff0c0129ea35fef
Crystal Finch <crystal.storm+finch@gmail.com>
2025-09-19 09:27:41 -0700

    Add playwright tests

---
+++ apps/client-e2e/src/home.spec.ts
@@ -0,0 +1,52 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('Home Page', () => {
+  test('has title', async ({ page }) => {
+    await page.goto('/');
+    expect(await page.locator('h1').innerText()).toContain('Home');
+  });
+});
+
+test.describe('Home Page Unauthenticated', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.route('**/api/auth/check-auth', (route) => {
+      route.fulfill({
+        status: 200,
+        contentType: 'application/json',
+        body: JSON.stringify({ ok: true, data: { user: null } }),
+        headers: { 'access-control-allow-origin': '*' },
+      });
+    });
+    await page.waitForLoadState('networkidle');
+
+    await page.goto('/');
+  });
+
+  test('has "Home" link', async ({ page }) => {
+    await expect(page.getByRole('link', { name: 'Home' })).toBeVisible();
+  });
+
+  test('has "Sign in" link', async ({ page }) => {
+    await expect(page.getByRole('link', { name: 'Sign in' })).toBeVisible();
+  });
+
+  test('has "Create account" link', async ({ page }) => {
+    await expect(
+      page.getByRole('link', { name: 'Create account' })
+    ).toBeVisible();
+  });
+});
+
+test.describe('Home Page Authenticated', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('/create-account');
+    await page.fill('input[name="email"]', 'test1@test.com');
+    await page.fill('input[name="password"]', 'asdfasdfasdfasdfasdfA1!');
+    await page.click('button[type="submit"]');
+    await page.waitForTimeout(5000);
+  });
+
+  test('has "Dashboard" link', async ({ page }) => {
+    await expect(page.getByRole('link', { name: 'Dashboard' })).toBeVisible();
+  });
+});
