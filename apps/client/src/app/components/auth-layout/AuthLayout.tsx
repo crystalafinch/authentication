8026499549b30de11fc46460e09b33ecb98d1239
Crystal Finch <crystal.storm+finch@gmail.com>
2025-09-19 10:20:38 -0700

    Apply gradient to auth layout

--- apps/client/src/app/components/auth-layout/AuthLayout.tsx
+++ apps/client/src/app/components/auth-layout/AuthLayout.tsx
@@ -2,7 +2,7 @@ function AuthLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="h-screen box-border flex gap-4 items-center justify-center">
       {children}
-      <div className="bg-gray-200 grow h-full w-2xl"></div>
+      <div className="bg-gradient-to-tr from-blue-200 to-pink-200 grow h-full w-2xl"></div>
     </div>
   );
 }
